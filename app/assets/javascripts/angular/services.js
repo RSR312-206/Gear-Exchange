c96517db29b01d06bcbf7615fa2795e4111b6e60
RSR312-206 <roccosummer@gmail.com>
2015-11-16 16:42:39 -0800

    new form complete

--- app/assets/javascripts/angular/services.js
+++ app/assets/javascripts/angular/services.js
@@ -8,14 +8,5 @@ exchangeApp.factory('Item', function($http) {
       Item.arr.push(item);
     })
   })
-
-  // Item.editItem = function(edit) {
-  //   $http.put("/items/" + item.id, item).success(function(data) {
-  //     console.log(data);
-  //   })
-  //   .error(function(data) {
-  //     console.log("error " + data);
-  //   });
-  // }
   return Item;
 })
