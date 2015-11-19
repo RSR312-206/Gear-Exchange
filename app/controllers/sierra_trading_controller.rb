f2eb75cca58704d6a9e7e4e4f8ca468e9f18412a
RSR312-206 <roccosummer@gmail.com>
2015-11-18 16:21:13 -0800

    fixed controller file

--- app/controllers/sierra_trading_controller.rb
+++ app/controllers/sierra_trading_controller.rb
@@ -1,7 +1,7 @@
 class SierraTradingController < ApplicationController
   def index
     search_params = params.permit(:search_model_type)
-    api_call = "http://api.sierratradingpost.com:80/api/1.0/products/search~#{search_params[:search_model_type]}/?api_key=#{ENV['SIERRA_TRADING_API'}]}"
+    api_call = "http://api.sierratradingpost.com:80/api/1.0/products/search~#{search_params[:search_model_type]}/?api_key=#{ENV['SIERRA_TRADING'}]}"
     price = Excon.get(api_call)
     render json: price.body
   end
